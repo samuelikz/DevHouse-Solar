94dcc129817bf4c7e0b379ecbc7de3299b39d158
samuel-n-silva <samuel_nunes-silva@estudante.sesisenai.org.br>
2023-05-06 22:30:58 -0300

    css dash boader e containe
    css dash boader e containe

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,7 +1,9 @@
+import Container from "../../components/template/Container/Container";
+
 export default function Dashboard(){
     return (
-        <>
+        <Container title="Dashboard">
             <h1>Pagina Dashboard</h1>
-        </>
+        </Container>
     )
 }
