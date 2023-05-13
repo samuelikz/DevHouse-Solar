929fbdfc0c13b9b2104064975a912fa91c262bd6
samue-n-silva <samuel_nunes-silva@estudante.sesisenai.org.br>
2023-05-13 16:41:38 -0300

    Cards do Dashboard Adicionado

--- src/components/index.js
+++ src/components/index.js
@@ -5,6 +5,7 @@ export { default as CadastroUnidades } from './organismos/CadastroUnidade/Cadast
 export { default as UnidadesList } from './organismos/Unidades/UnidadesList';
 export { default as CadastroMensal } from './organismos/CadastroMensal/CadastroMensal';
 export { default as LoginPage } from './moleculas/LoginPage/LoginPage';
+export { default as Cards} from './organismos/Cards/Cards';    
 
 
 
